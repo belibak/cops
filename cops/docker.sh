041d6907bb566de3f869bb128535cbe8d17add15
Alexander Rusetski <alex.rus@core-tech.ru>
2017-07-01 10:42:23 +0300

    uwsgi

--- cops/docker.sh
+++ cops/docker.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-python /cops/run.py
+#python /cops/run.py
+cd /cops/cops && uwsgi --socket 0.0.0.0:5000 --protocol=http -w wsgi:app
+
 
 #service supervisor start
 #service nginx start
